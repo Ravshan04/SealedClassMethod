0be653da06c5064bf0fce2353df6433427002bc6
Ravshanbek-Nazarov <ulugbeknazarov068@gmail.com>
2025-03-22 00:58:27 +0500

    Add project files.

---
+++ EncapsulationLessonTasks/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_Class
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //....
+        }
+
+        public class Asosiy
+        {
+            // voris bo'ladigan method
+            public virtual int Add(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+        //asosiy classdan voris olish
+        public class Voris1 : Asosiy
+        {
+            //voris bo'lgan methodni sealed qilish
+            public sealed override int Add(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+        public class Inherit : Voris1
+        {
+            public override int Add(int a, int b)
+            {
+                return base.Add(a, b);
+            }
+        }
+    }
+}
---
+++ EncapsulationLessonTasks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationLessonTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(45, 5);
+            car.CarAction();
+            car.FuelFill(5);
+            Console.ReadKey();
+        }
+    }
+}
